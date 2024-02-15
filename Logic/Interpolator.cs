487c04d62e54edec403f8d53cb2fb31dfcbaf968
Somewhere <likeaboss13371@gmail.com>
2024-02-15 22:16:58 +0200

    New Interpolation Algorythm Added

--- Logic/Interpolator.cs
+++ Logic/Interpolator.cs
@@ -5,37 +5,66 @@ namespace NewtonInterpolationSolver.Logic
     // Клас для реалізації інтерполяційних методів
     public class Interpolator
     {
-        // Перша інтерполяційна формула Ньютона для равностоящих узлів
+        // Перша інтерполяційна формула Ньютона для равностоячих узлів
         public static double NewtonFirstInterpolation(double[] x, double[] y, double value)
         {
+            // Кількість вузлів
             int n = x.Length;
-            double result = 0;
 
-            // Ітерація по узлам
+            TextViewer.ChangeColor("\nІніціалізація\n", "blue");
+            // Ініціалізація результату значенням у першому вузлі
+            double result = y[0];
+            Console.WriteLine("Результат після ініціалізації: " + result);
+
+            // Масив для збереження різницевих коефіцієнтів
+            double[] diffCoeff = new double[n];
+
+            // Ініціалізація масиву різницевих коефіцієнтів початковими значеннями
             for (int i = 0; i < n; i++)
             {
-                double term = y[i];
-                string formula = $"{y[i]}";
-
-                // Обчислення члену формули для даного узла
-                for (int j = 0; j < i; j++)
-                {
-                    term *= (value - x[j]) / (x[i] - x[j]);
-                    formula += $" * ({value} - {x[j]}) / ({x[i]} - {x[j]})";
-                }
-
-                // Додавання до результату
-                result += term;
-
-                // Вивід проміжного результату
-                Console.WriteLine($"\nПроміжний результат для ітерації {i + 1}:");
-                TextViewer.ChangeColor($"\t\n{formula} = {result}", "magenta");
+                diffCoeff[i] = y[i];
             }
 
+            TextViewer.ChangeColor("\nОбчислення різнецевих коефіцієнтів\n", "blue");
+            // Обчислення різницевих коефіцієнтів
+            for (int i = 1; i < n; i++)
+            {
+                TextViewer.ChangeColor($"Крок {i}\n", "blue");
+                for (int j = n - 1; j >= i; j--)
+                {
+                    // Розрахунок різницевого коефіцієнта за формулою (f(x_j) - f(x_{j-1})) / (x_j - x_{j-i})
+                    double oldDiffCoeff = diffCoeff[j];
+                    diffCoeff[j] = (diffCoeff[j] - diffCoeff[j - 1]) / (x[j] - x[j - i]);
+                    TextViewer.ChangeColor($"\t({oldDiffCoeff} - {diffCoeff[j]}) / ({x[j]} - {x[j - i]}) = {diffCoeff[j]}\n", "magenta");
+                }
+                Console.WriteLine($"Різницеві коефіцієнти після кроку {i}:\n[{string.Join(", ", diffCoeff)}]\n");
+            }
+
+            TextViewer.ChangeColor("Обчислення значення інтерполяції за формулою Ньютона\n", "blue");
+            // Обчислення значення інтерполяції за формулою Ньютона
+            for (int i = 1; i < n; i++)
+            {
+                TextViewer.ChangeColor($"\nДоданки для кроку {i}:\n", "blue");
+                double term = diffCoeff[i];
+
+                // Обчислення кожного доданку за формулою term *= (value - x[j])
+                for (int j = 0; j < i; j++)
+                {
+                    TextViewer.ChangeColor($"\tДоданок {j + 1}: {term} * ({value} - {x[j]}) = ", "magenta");
+                    term *= (value - x[j]);
+                    TextViewer.ChangeColor($"\t = {term}\n", "magenta");
+                }
+
+                // Додавання доданку до результату
+                result += term;
+            }
+
+            TextViewer.ChangeColor("Сума усіх отриманих додатків\n", "blue");
+            // Повернення результату інтерполяції
             return result;
         }
 
-        // Друга інтерполяційна формула Ньютона для равностоящих узлів
+        // Друга інтерполяційна формула Ньютона для равностоячих узлів
         public static double NewtonSecondInterpolation(double[] x, double[] y, double value)
         {
             // Отримання кількості точок для інтерполяції
@@ -80,6 +109,5 @@ namespace NewtonInterpolationSolver.Logic
 
             return result;
         }
-
     }
 }
